48e0e7480ec3ed7bb8226bed8056be6ee25ed493
Heikki Ritola <heikki@oneten.cx>
2015-10-12 21:52:18 +0300

    Arpeggiator

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var _ = require('lodash')
 var $ = require('jquery')
 var Bacon = require('baconjs')
 var bjq = require("bacon-jquery-bindings")
@@ -41,10 +42,54 @@ function openMidiInputPort(id) {
     })
 }
 
+var midiData = midiMessages
+  .map(function(e) { return e.data })
+  .filter(filterMidiChannel(0))
+
+var notesPressed = midiData
+  .scan([], function(previous, data) {
+    if (isNoteOn(data)) {
+      return _.union(previous, [data[1]])
+    } else if (isNoteOff(data)) {
+      return _.without(previous, data[1])
+    } else {
+      return previous
+    }
+  }).toProperty()
+
+notesPressed
+  .sample(200)
+  .scan({}, function(previous, notes) {
+    if (notes.length === 0) return {}
+
+    var index = nextIndex(previous.index)
+    return {
+      index: index,
+      note: notes[index]
+    }
+  })
+  .map('.note')
+  .filter(function(x) { return x })
+  .log()
+
+function nextIndex(index) {
+  return (index === undefined) ? 0 : (index + 1) % notes.length
+}
+
 function midiMessageReceived(event) {
   midiMessages.push(event)
 }
 
-midiMessages
-  .map(function(e) { return e.data })
-  .log()
+function filterMidiChannel(channel) {
+  return function (data) {
+    return (data[0] & 0x0f) === channel
+  }
+}
+
+function isNoteOn(data) {
+  return (data[0] & 0xf0) === 0x90
+}
+
+function isNoteOff(data) {
+  return (data[0] & 0xf0) === 0x80
+}
